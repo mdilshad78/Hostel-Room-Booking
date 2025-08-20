fe008f7015a9ada275f60c6609a3aa7e69d6112e
dilshad <mohddilshad3743@gmail.com>
2025-08-20 11:30:29 +0530

    changes

--- hostel_react/src/Users_Login/Show.jsx
+++ hostel_react/src/Users_Login/Show.jsx
@@ -2,46 +2,46 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
 export default function Show() {
-    const [data,setData]=useState([])
+    const [data, setData] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         getdata();
     })
 
-    const getdata=()=>{
-        axios.get("http://localhost:3001/show").then((result)=>{
+    const getdata = () => {
+        axios.get("http://hostel-room-booking-frontend.vercel.app/show").then((result) => {
             setData(result.data)
-        }).catch((err)=>{
+        }).catch((err) => {
             console.log(err);
         })
     }
-  return (
-    <>
-        <table className='table'>
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Password</th>
-                </tr>
-            </thead>
+    return (
+        <>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Password</th>
+                    </tr>
+                </thead>
 
-            <tbody>
-                {
-                    data.map((item,index)=>{
-                        <tr>
-                            <td>{index+1}</td>
-                            <td>{item.name}</td>
-                            <td>{item.email}</td>
-                            <td>{item.phone}</td>
-                            <td>{item.password}</td>
-                        </tr>
-                    })
-                }
-            </tbody>
-        </table>
-    </>
-  )
+                <tbody>
+                    {
+                        data.map((item, index) => {
+                            <tr>
+                                <td>{index + 1}</td>
+                                <td>{item.name}</td>
+                                <td>{item.email}</td>
+                                <td>{item.phone}</td>
+                                <td>{item.password}</td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+        </>
+    )
 }
